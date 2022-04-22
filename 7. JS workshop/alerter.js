d38504fa4174b94a63f250527d28fac2c62c7c07
Aniket Rai <aniketrai025@gmail.com>
2022-04-22 13:19:53 +1200

    JS workshop completed :memo:

--- 7. JS workshop/alerter.js
+++ 7. JS workshop/alerter.js
@@ -1,3 +1,5 @@
 const sendAlert = () => {
+    const element = document.getElementById("general_kenobi");
+    element.getAttribute("hidden") ? element.removeAttribute("hidden") : element.setAttribute("hidden", true)
     alert('Why Hello There');
 }
